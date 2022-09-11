93536edbccc85cff122adda593f206039063636a
APTEMmm <artemqrtz@gmail.com>
2022-09-11 15:19:21 +0300

    Added validations for subscription form

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def event_owner?(current_user, event)
+    current_user == event.user
+  end
+
+  def already_subscribed?(user, event)
+    user.present? ? user.subscriptions.find_by(event_id: event.id).present? : false
+  end
 end
