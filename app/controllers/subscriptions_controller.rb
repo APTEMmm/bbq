8f424bf20d239c113a7ac5f2df7942873f938aee
APTEMmm <artemqrtz@gmail.com>
2022-09-11 17:49:21 +0300

    Added restriction for subscriptions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -3,10 +3,12 @@ class SubscriptionsController < ApplicationController
   before_action :set_subscription, only: [:destroy]
 
   def create
-    @new_subscription = @event.subscriptions.build(subscription_params)
-    @new_subscription.user = current_user
+    unless User.find_by(email: params[:subscription][:user_email]).present?
+      @new_subscription = @event.subscriptions.build(subscription_params)
+      @new_subscription.user = current_user
+    end
 
-    if @new_subscription.save
+    if @new_subscription&.save
       redirect_to @event, notice: I18n.t('controllers.subscriptions.created')
     else
       render 'events/show', alert: I18n.t('controllers.subscriptions.error')
