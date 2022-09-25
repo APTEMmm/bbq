e74641b064110f0c36cb34a9986be4c4abd925fa
APTEMmm <artemqrtz@gmail.com>
2022-09-26 00:11:55 +0300

    Updated rbenv path for capistarno

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ append :linked_files, "config/database.yml", 'config/master.key'
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "$PATH" }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
